53c343b7636a0923a0f042985e60c3eec8498264
CatoReinante <catoreinante@gmail.com>
2025-09-03 14:13:18 -0300

    create api.ts

---
+++ mini-blog/src/hooks/Api.ts
@@ -0,0 +1,12 @@
+const BASE_URL = "http://localhost:3000/api";
+
+export async function loginApi(username: string, password: string) {
+  const response = await fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  return response.json();
+}
